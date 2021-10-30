34fd1d446f470e7eff8dc3c02437f6af7d4292b5
howe-carol2019 <how16021@byui.edu>
2021-10-30 10:11:47 -0700

    s

--- week7/main.js
+++
@@ -1,6 +0,0 @@
-import Hikes from "./Hikes.js";
-
-const hikesObj = new Hikes();
-    
-//on load grab the array and insert it into the page
-window.addEventListener("load", () => {hikesObj.showHikeList()});
